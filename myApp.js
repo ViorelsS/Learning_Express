bdade71eb180a1c641737a1c015db6b57fcbb0f3
Viorel Strogoteanu <viorelss@icloud.com>
2023-07-18 18:39:13 +0200

    [Dummy] Added file response to get method

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+// app.METHOD(PATH, HANDLER) --> the handler is a function that Express calls when the route is matched.
+
+absolutePath = __dirname + '/views/index.html'
+
+// app.get("/", (req, res) =>res.send("Hello Express"));
+app.get("/", (req, res) => res.sendFile(absolutePath));
 console.log("Hello World");
 
 

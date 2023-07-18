bf9beeed9cddee3b37f87cfe878d17420527d639
Viorel Strogoteanu <viorelss@icloud.com>
2023-07-18 19:22:50 +0200

    [General] Added req info logger

--- myApp.js
+++ myApp.js
@@ -1,51 +1,29 @@
-let express = require('express');
+require("dotenv").config();
+let express = require("express");
 let app = express();
 
 // app.METHOD(PATH, HANDLER) --> the handler is a function that Express calls when the route is matched.
 
-absolutePath = __dirname + '/views/index.html'
+absolutePath = __dirname + "/views/index.html";
+
+app.use((req, res, next)=>{
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+})
 
 // app.get("/", (req, res) =>res.send("Hello Express"));
 app.get("/", (req, res) => res.sendFile(absolutePath));
-console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", (req,res)=>res.json({"message": "Hello json"}));
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    return res.json({ message: "HELLO JSON" });
+  } else {
+    return res.json({ message: "Hello json" });
+  }
+});
 
 
+module.exports = app;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
